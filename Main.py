e30395486fec2c9ff195427fcf016a219564b3f6
Sam Sims <samsimss98@gmail.com>
2018-08-13 15:54:09 +0100

    Added ability to select a webdriver, along with a config fike
    Webdriver can now be chosen between chrome or firefox and a path specified as well as headless mode.
    Settings are stored in config.ini
    config.ini has foundations to enable logging

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from configparser import ConfigParser
 import time
 import csv
 
@@ -12,21 +14,74 @@ import csv
 # TODO Implement command line arguments (-t time to run for continuous operation, -MB for megabytes/bits etc)
 # TODO Exception handling
 
-class SpeedTest:
-    # TODO Implement a config file to point to the geckodriver binary?
+class Config:
 
     def __init__(self):
+        config = ConfigParser()
+        config.read('config.ini')
+        self.driver_path = config.get('DRIVER', 'path')
+        self.driver_type = config.get('DRIVER', 'type')
+        self.enable_headless = config.get('DRIVER', 'enable_headless')
+        self.enable_log = config.get('LOG', 'enable_log')
+
+
+class SpeedTest:
+
+    def __init__(self, config:Config):
         self.download = ""
         self.upload = ""
         self.ping = ""
+        self.config = config
         self.driver = None
         self.wait = None
 
         try:
+            # Create options for both firefox and chrome
             options = Options()
-            options.add_argument("--headless")
-            self.driver = webdriver.Firefox()  # Starts firefox with options defined above
+            options.add_argument("--headless")  # --headless
+            options_chrome = webdriver.ChromeOptions()
+            options_chrome.add_argument('headless')
+            # Check if a driver type is specified
+            if self.config.driver_type is not None:
+                # Check if firefox is specified
+                if self.config.driver_type == "firefox":
+                    # If firefox is specified check if a path for the binary is specified
+                    if self.config.driver_path is not None:
+                        # If headless is true
+                        if self.config.enable_headless == "true":
+                            self.driver = webdriver.Firefox(executable_path=config.driver_path, firefox_options=options)
+                        else:
+                            self.driver = webdriver.Firefox(executable_path=config.driver_path)
+                    # if no path is specified try default location (root directory)
+                    else:
+                        # Check if headless is true
+                        if self.config.enable_headless == "true":
+                            self.driver = webdriver.Firefox(firefox_options=options)
+                        else:
+                            self.driver = webdriver.Firefox()
+                # Check if chrome is specified in the config
+                elif self.config.driver_type == "chrome":
+                    # If chrome is specified check if a path for the binary is specified
+                    if self.config.driver_path is not None:
+                        # Check if headless is true
+                        if self.config.enable_headless == "true":
+                            self.driver = webdriver.Chrome(executable_path=config.driver_path, chrome_options=options_chrome)
+                        else:
+                            self.driver = webdriver.Chrome(executable_path=config.driver_path)
+                    # If no path is specified try default location (root directory)
+                    else:
+                        # Check if headless is true
+                        if self.config.enable_headless == "true":
+                            self.driver.webdriver.Chrome(chrome_options=options_chrome)
+                        else:
+                            self.driver = webdriver.Chrome()
+                else:
+                    print("Please specify a valid driver type!")
+            else:
+                print("Some kind of error loading the config")
+
             self.wait = WebDriverWait(self.driver, 5)
+
         except WebDriverException as e:
             print("Driver creation failed: ", str(e))
 
@@ -42,6 +97,11 @@ class SpeedTest:
         self.ping = str(self.driver.find_element_by_xpath("//span[contains(@class, 'result-data-large number result-data-value ping-speed')]").text)
         self.download = str(self.driver.find_element_by_xpath("//span[contains(@class, 'result-data-large number result-data-value download-speed')]").text)
         self.upload = str(self.driver.find_element_by_xpath("//span[contains(@class, 'result-data-large number result-data-value upload-speed')]").text)
+
+    # Deconstructor to dispose of the webdriver
+    def __del__(self):
+        if self.driver is not None:
+            self.driver.quit()
 
 
 class Output:
@@ -62,11 +122,13 @@ class Output:
 
 # This is for testing only
 def main():
-    st = SpeedTest()
+    config = Config()
+    st = SpeedTest(config)
     st.run_test()
     st.store_values()
     output = Output(st)
     output.write_csv()
+
 
 
 if __name__ == "__main__":
